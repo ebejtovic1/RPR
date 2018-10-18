fbaf12c5284b1f2bf2f90de65c9cb89e0af9144c
ebejtovic1 <ebejtovic1@etf.unsa.ba>
2018-10-18 22:05:52 +0200

    .

---
+++ Tutorijal 1/t1z4/src/ba/unsa/etf/treci/Program.java
@@ -0,0 +1,15 @@
+package ba.unsa.etf.treci;
+
+public class Program {
+    public static void main(String[] args) {
+        Student s = new Student("Elma","Bejtović",17770);
+        Student s1 = new Student("Elmaaaa","Bejdddtović",1722770);
+        Predmet p =new Predmet("Matematija",10,10);
+        p.upisiStudenta(s);
+        p.upisiStudenta(s1);
+        p.ispisiStudenta(s1);
+        System.out.println(p);
+        System.exit(0);
+
+    }
+}
